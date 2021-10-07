f622996d8aa03f329afe8a5df10bdccb292ee32c
daniklubohinec <danik.lubohinec@gmail.com>
2021-10-07 15:14:25 +0300

    - build 11

--- EyezonSDK-Test/EyezonSDK-Test/ViewController.swift
+++ EyezonSDK-Test/EyezonSDK-Test/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 import EyezonSDK
 
+var apnToken = ""
+var bundleID = ""
+
 class ViewController: UIViewController {
+    
     private enum Constants {
-        static let EYEZON_WIDGET_URL =
-            "https://storage.googleapis.com/eyezonfortest/test-widget/webview.html?eyezon&putInCart=true&eyezonRegion=sandbox&language=ru&target=SKU-1&title=Samsung%20Television&name=Test&phone=%2B3801111111111&email=test@test.test&clientId=test123&region=Brest"
+        static let EYEZON_WIDGET_URL = "https://storage.googleapis.com/eyezonfortest/test-widget/webview.html?eyezon&businessId=5d63fe246c2590002eecef83&language=ru&buttonId=5ec26f248107de3797f0807c&target=SKU-1&title=Samsung%20Television&apnToken=\(apnToken)&application=\(bundleID)&eyezonRegion=sandbox"
         
         static let EYEZON_BUSINESS_ID = "5d63fe246c2590002eecef83"
         static let EYEZON_BUTTON_ID = "5ec26f248107de3797f0807c"
@@ -40,8 +43,14 @@ class ViewController: UIViewController {
     
     @objc
     private func openEyezon() {
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        apnToken = delegate?.pushToken ?? "Device token is empty"
+        
+        bundleID = Bundle.main.bundleIdentifier ?? "BundleID is empty"
+        
         Eyezon.instance.initSdk(area: selectedServer) { [weak self, predefinedData] in
             let eyezonWebViewController = Eyezon.instance.openButton(data: predefinedData, broadcastReceiver: self)
+            print(Constants.EYEZON_WIDGET_URL)
             self?.navigationController?.pushViewController(eyezonWebViewController, animated: true)
         }
     }
@@ -53,22 +62,6 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: EyezonBroadcastReceiver {
-    func onNewMessage(message: EyezonMessage) {
-        print(#function)
-    }
-    
-    func onDialogCreated(dialogId: String) {
-        print(#function)
-    }
-    
-    func onDialogDeleted(dialogId: String) {
-        print(#function)
-    }
-    
-    func onDialogReturned(dialogId: String) {
-        print(#function)
-    }
-    
     func onConsoleEvent(eventName: String, event: [String: Any]) {
         print(#function, " \(eventName)")
     }
